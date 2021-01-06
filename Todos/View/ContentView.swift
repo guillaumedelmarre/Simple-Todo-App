b8e618293613d2fdace25e182c5c9a312432695f
Guillaume Delmarre <60462051+guillaumedelmarre@users.noreply.github.com>
2021-01-06 08:24:37 +0800

    Initial setup

---
+++ Todos/View/ContentView.swift
@@ -0,0 +1,115 @@
+//
+//  ContentView.swift
+//  Todo App
+//
+//  Created by Guillaume Delmarre on 1/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - PROPERTIES
+    
+    @Environment(\.managedObjectContext) var managedObjectContext
+    
+    @FetchRequest(entity: Todo.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Todo.name, ascending: true)]) var todos: FetchedResults<Todo>
+    
+    @State private var showingAddTodoView: Bool = false
+    @State private var animatingButton: Bool = false
+    
+    // MARK: - BODY
+    var body: some View {
+        NavigationView {
+            ZStack {
+                List {
+                    ForEach(self.todos, id: \.self) { todo in
+                        HStack {
+                            Text(todo.name ?? "Unknown")
+                            
+                            Spacer()
+                            
+                            Text(todo.priority ?? "Unkwnown")
+                        }
+                    } // END: FOR EACH
+                    .onDelete(perform: deleteTodo)
+                    
+                } // END: LIST
+                .navigationBarTitle("Todo", displayMode: .inline)
+                .navigationBarItems(
+                    leading: EditButton(),
+                    trailing:
+                    Button(action: {
+                    //Show add todo view
+                        self.showingAddTodoView.toggle()
+                    }) {
+                        Image(systemName: "plus")
+                    } // END: ADD BUTTON
+                        .sheet(isPresented: $showingAddTodoView) {
+                            AddTodoView().environment(\.managedObjectContext, self.managedObjectContext)
+                        }
+            )
+                // MARK: - NO TODO ITEMS
+                if todos.count == 0 {
+                    EmptyListView()
+                }
+            } // END: ZSTACK
+            .sheet(isPresented: $showingAddTodoView) {
+                AddTodoView().environment(\.managedObjectContext, self.managedObjectContext)
+            }
+            .overlay(
+                ZStack {
+                    Group {
+                        Circle()
+                            .fill(Color.blue)
+                            .opacity(0.2)
+                            .frame(width: 68, height: 68, alignment: .center)
+                        Circle()
+                            .fill(Color.blue)
+                            .opacity(0.1)
+                            .frame(width: 88, height: 88, alignment: .center)
+                    }
+
+                    
+                    Button(action: {
+                        self.showingAddTodoView.toggle()
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .background(Circle().fill(Color("ColorBase")))
+                            .frame(width: 48, height: 48, alignment: .center)
+                    } // END: BUTTON
+                    .onAppear(perform: {
+                        self.animatingButton.toggle()
+                    })
+                } // END: ZSTACK
+                .padding(.bottom, 15)
+                .padding(.trailing, 15)
+                ,alignment: .bottomTrailing
+            )
+        } // END: NAVIGATION
+    }
+    
+    // MARK: - FUNCTIONS
+    
+    private func deleteTodo(at offsets: IndexSet) {
+        for index in offsets {
+            let todo = todos[index]
+            managedObjectContext.delete(todo)
+            
+            do {
+                try managedObjectContext.save()
+            } catch {
+                print(error)
+            }
+        }
+    }
+}
+// MARK: - PREVIEW
+struct ContentView_Previews: PreviewProvider {
+static var previews: some View {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    return ContentView()
+        .environment(\.managedObjectContext, context)
+    }
+}
